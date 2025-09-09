a0c95cd96e9dd1ba3b15026ac230ec7090c475f5
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-09-09 00:49:30 -0500

    added public route

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -27,3 +27,26 @@ resource "aws_nat_gateway" "nat" {
   })
 }
 
+# Public route
+resource "aws_route_table" "public"{
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(
+    var.tags, 
+    {
+      Name = "${var.vpc_name}-public-rt"
+    }
+  )
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+
+  subnet_id = element(aws_subnet.public[*].id,count.index)
+  route_table_id = aws_route_table.public.id
+}
