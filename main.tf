e9334f66841fd9615e97a0bfeed1a065fb1880d4
Rahul Nagaraju <Rahul Nagaraju>
2025-09-09 12:50:33 -0500

    Terraform fmt on root folder

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
-provider "aws"{
-    region = var.region
+provider "aws" {
+  region = var.region
 }
 
-module "vpc"{
-    source = "./modules/vpc"
+module "vpc" {
+  source = "./modules/vpc"
 
-    vpc_cidr         = var.vpc_cidr
-    azs              = var.azs
-    public_subnets   = var.public_subnets
-    private_subnets  = var.private_subnets
-    db_subnets       = var.db_subnets
-    vpc_name         = var.vpc_name
-    enable_nat       = true
-    tags             = var.tags
+  vpc_cidr        = var.vpc_cidr
+  azs             = var.azs
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  db_subnets      = var.db_subnets
+  vpc_name        = var.vpc_name
+  enable_nat      = true
+  tags            = var.tags
 
 }
--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value = module.vpc.vpc_id
+  value       = module.vpc.vpc_id
 }
 
-output "private_subnets"{
-    description = "Private subnets"
-    value = module.vpc.private_subnets
+output "private_subnets" {
+  description = "Private subnets"
+  value       = module.vpc.private_subnets
 
-    sensitive   = true
+  sensitive = true
 }
 
-output "public_subnets"{
-    description = "Public subnets"
-    value = module.vpc.public_subnets
+output "public_subnets" {
+  description = "Public subnets"
+  value       = module.vpc.public_subnets
 
-    sensitive   = true
+  sensitive = true
 }
 
-output "db_subnets"{
-    description = "DB subnets"
-    value = module.vpc.db_subnets
+output "db_subnets" {
+  description = "DB subnets"
+  value       = module.vpc.db_subnets
 
-    sensitive   = true
+  sensitive = true
 }
