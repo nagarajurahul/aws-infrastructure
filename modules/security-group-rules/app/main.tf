ec584ff7bc1402a28e9e19d10bdb61bf732d44b0
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-09-09 18:17:44 -0500

    added sg-rules for app

---
+++ modules/security-group-rules/app/main.tf
@@ -0,0 +1,18 @@
+resource "aws_vpc_security_group_ingress_rule" "app_from_alb" {
+  security_group_id = var.security_group_id
+  referenced_security_group_id = var.source_security_group_id
+  from_port         = var.app_port
+  to_port           = var.app_port
+  ip_protocol       = "tcp"
+  description       = "Allow inbound traffic from ALB"
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "all_ipv4" {
+  security_group_id = var.security_group_id
+  # cidr_ipv4         = "0.0.0.0/0"
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "-1"
+}
---
+++ modules/security-group-rules/app/variables.tf
@@ -0,0 +1,9 @@
+variable "security_group_id" {
+  
+}
+variable "source_security_group_id" {
+  
+}
+variable "backend_port" {
+    default = 3000
+}
