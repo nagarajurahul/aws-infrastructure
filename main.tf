65be96686ac88df4570fb47bd4a991543a4c31d9
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-09-09 13:06:05 -0500

    add missing variables

--- main.tf
+++ main.tf
@@ -5,13 +5,14 @@ provider "aws" {
 module "vpc" {
   source = "./modules/vpc"
 
+  region          = var.region
   vpc_cidr        = var.vpc_cidr
   azs             = var.azs
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
   db_subnets      = var.db_subnets
   vpc_name        = var.vpc_name
-  enable_nat      = true
+  enable_nat_gateway      = true
   tags            = var.tags
 
 }
