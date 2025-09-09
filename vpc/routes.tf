10675415e047463b301f02e349f64cf37c8d8dfc
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-09-09 00:59:05 -0500

    add private route

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -12,12 +12,12 @@ resource "aws_internet_gateway" "igw" {
 
 # NAT Gateways (One per AZ if enabled)
 resource "aws_eip" "nat" {
-  count = var.enable_nat ? length(var.public_subnets) : 0
+  count = var.enable_nat_gateway ? length(var.public_subnets) : 0
   vpc   = true
 }
 
 resource "aws_nat_gateway" "nat" {
-  count         = var.enable_nat ? length(var.public_subnets) : 0
+  count         = var.enable_nat_gateway ? length(var.public_subnets) : 0
   
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
@@ -49,4 +49,32 @@ resource "aws_route_table_association" "public" {
 
   subnet_id = element(aws_subnet.public[*].id,count.index)
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  count = length(var.private_subnets)
+
+  vpc_id = aws_vpc.main.id
+
+  tags= merge(
+    var.tags, 
+    {
+      Name = "${var.vpc_name}-private-rt"
+    }
+  )
+}
+
+resource "aws_route" "private_nat_gateway" {
+  count = var.enable_nat_gateway ? length(aws_route_table.private) : 0
+
+  route_table_id         = aws_route_table.private[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.this[*].id, count.index)
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(aws_subnet.private)
+
+  subnet_id = element(aws_subnet.private[*].id,count.index)
+  route_table_id = aws_route_table.private[count.index].id
 }
