77a420b5b4861e7d1345a8e8d42e9777a3bbad74
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-09-09 00:21:33 -0500

    add variables.tf

---
+++ vpc/variables.tf
@@ -0,0 +1,33 @@
+variable "region"{
+    type = string
+    description = "The AWS Region where VPC will be deployed"
+
+    validation {
+      condition = can(regex("^[a-z]{2}-[a-z]+-[0-9]{1}$", var.region))
+      error_message = "Please enter a valid AWS region (e.g., us-east-1, us-west-2, eu-central-1)"
+    }
+}
+
+variable "vpc_cidr"{
+    type = string
+    description = "CIDR block for the VPC"
+}
+
+variable "vpc_name"{
+    type = string
+    description = "Name of the VPC"
+}
+
+variable "tags" {
+  description = "Common tags for all resources"
+  type        = map(string)
+
+  default     = { "Environment" =  "Production" }
+}
+
+variable "enable_ipv6" {
+  description = "Enable ipv6 for VPC and subnets"
+  type = bool
+
+  default = false
+}
