c030103836588f77ec5a3bc570466b992f335800
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-09-09 19:51:59 -0500

    correct cidr syntax in sg-rules

--- modules/security-group-rules/alb/main.tf
+++ modules/security-group-rules/alb/main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc_security_group_ingress_rule" "https_ipv4" {
 
 resource "aws_vpc_security_group_egress_rule" "all_ipv4" {
   security_group_id = var.security_group_id
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_ipv4 = "0.0.0.0/0"
   from_port         = 0
   to_port           = 0
   ip_protocol       = "-1"
