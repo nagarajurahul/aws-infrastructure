fcd8e2453b4f369507bcf027f3e98b9a7da2694d
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-09-09 20:25:02 -0500

    organise variables inside root module

--- main.tf
+++ main.tf
@@ -7,14 +7,13 @@ module "vpc" {
 
   region             = var.region
   vpc_cidr           = var.vpc_cidr
+  vpc_name           = var.vpc_name
+  tags               = var.tags
+  enable_nat_gateway = true
   azs                = var.azs
   public_subnets     = var.public_subnets
   private_subnets    = var.private_subnets
   db_subnets         = var.db_subnets
-  vpc_name           = var.vpc_name
-  enable_nat_gateway = true
-  tags               = var.tags
-
 }
 
 module "alb" {
