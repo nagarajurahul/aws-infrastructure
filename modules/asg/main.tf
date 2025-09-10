29a0f8d8260325c1d2810abc8094f4bb48f31090
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-09-09 23:40:30 -0500

    add asg module

---
+++ modules/asg/main.tf
@@ -0,0 +1,38 @@
+module "launch_template" {
+  source = "../launch-template"
+
+  tags                      = var.tags
+  type                      = var.type
+  ami_id                    = var.ami_id
+  instance_type             = var.instance_type
+  vpc_security_group_ids    = var.vpc_security_group_ids
+  iam_instance_profile_name = var.iam_instance_profile_name
+  hibernation_mode          = var.hibernation_mode
+  key_name                  = var.key_name
+  user_data                 = var.user_data
+}
+
+
+resource "aws_autoscaling_group" "asg" {
+  name = "${var.asg_name}-asg"
+
+  # availability_zones = var.azs
+  vpc_zone_indentifier = [for subnet in var.subnets : subnet.id]
+  desired_capacity   = var.desired_capacity
+  max_size           = var.max_size
+  min_size           = var.min_size
+
+  launch_template {
+    id      = module.launch_template.launch_template_id
+    version = "$Latest"
+  }
+
+  target_group_arns = var.target_group_arns
+
+  tag {
+    key                 = "Name"
+    value               = "${var.asg_name}-asg"
+    propagate_at_launch = true
+  }
+
+}
