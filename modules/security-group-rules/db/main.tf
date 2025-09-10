c315ba609b92046b030ae837aefd69d4f9288ebe
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-09-09 20:44:24 -0500

    update variable names in sg-rules module

--- modules/security-group-rules/db/main.tf
+++ modules/security-group-rules/db/main.tf
@@ -9,8 +9,7 @@ resource "aws_vpc_security_group_ingress_rule" "db_from_app" {
 
 resource "aws_vpc_security_group_egress_rule" "all_ipv4" {
   security_group_id = var.security_group_id
-  # cidr_ipv4         = "0.0.0.0/0"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 0
   to_port           = 0
   ip_protocol       = "-1"
